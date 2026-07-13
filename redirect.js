9b1b32f6b3766e8bdc8bf0fd0a921e69b18f4898
Anahi0920 <anahifelix9@gmail.com>
2026-07-13 16:22:24 -0600

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,7 @@
-
+const parametros=new URLSearchParams(window.location.search);
+const id=parametros.get("id");
+if(id){
+localStorage.setItem("INVITADO",id);
+}
+alert("ID guardado correctamente: "+id);
+//Aquí después redireccionaremos a Canva
