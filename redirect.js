1099a506aa3cadbd77e270c539b98b0d8ee17fc7
Anahi0920 <anahifelix9@gmail.com>
2026-07-13 19:13:19 -0600

    Save ID to localStorage and notify user
    Store the 'id' parameter in localStorage and alert the user.

--- redirect.js
+++ redirect.js
@@ -1,7 +1,13 @@
 const parametros=new URLSearchParams(window.location.search);
+
 const id=parametros.get("id");
+
 if(id){
+
 localStorage.setItem("INVITADO",id);
+
 }
+
 alert("ID guardado correctamente: "+id);
+
 //Aquí después redireccionaremos a Canva
